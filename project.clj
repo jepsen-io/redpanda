a9a52cbb7c15e99654da0d8febf941778d1450e4
Aphyr <aphyr@aphyr.com>
2022-01-31 12:43:33 -0500

    Bump cheshire, jepsen, clojure deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/jepsen-io/redpanda"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [clj-http "3.12.3"]
-                 [cheshire "5.10.1"]
-                 [jepsen "0.2.6-SNAPSHOT"]
+                 [cheshire "5.10.2"]
+                 [jepsen "0.2.6"]
                  [org.apache.kafka/kafka-clients "3.0.0"]]
   :main jepsen.redpanda.core
   :test-selectors {:focus :focus}
