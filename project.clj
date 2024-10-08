d0dab441398ca7e8d07f18f26527156295855dfe
Kyle Kingsbury <aphyr@aphyr.com>
2024-10-08 17:04:52 -0500

    Clojure 1.12

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jepsen-io/redpanda"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [clj-http "3.13.0"
                   :exclusions [riddley]]
                  [cheshire "5.13.0"]
